20417567a9bddad8100d047aa3f96e7d30d7e558
Berk <alexeym00@gmail.com>
2020-01-20 21:47:35 +0200

    attempt to add recorder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Recorder from './Recorder.js';
 
 class App extends Component {
   render() {
@@ -14,9 +16,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button variant="contained" color="primary" onClick={btnAlert}>
-          Hello!
-        </Button>
+        <ButtonGroup color="primary" aria-label="outlined primary button group">
+          <Button variant="contained" color="primary"  id="record" onClick={btnAlert}>
+            Record
+          </Button>
+          <Button variant="contained" color="secondary" id="stop" onClick={btnAlert}>
+            Stop
+          </Button>
+          <Button variant="contained" color="primary"  id="play" onClick={btnAlert}>
+            Play
+          </Button>
+          <Button variant="contained" color="primary"  id="load" onClick={btnAlert}>
+            Load
+          </Button>
+        </ButtonGroup>
       </div>
     );
   }
@@ -24,8 +37,7 @@ class App extends Component {
 }
 
 function btnAlert() {
-  alert("it Works!");
+  alert("it Works! " + Recorder);
 }
-
 
 export default App;
