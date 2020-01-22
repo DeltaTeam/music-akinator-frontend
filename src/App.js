d73719701ce032555682bbee480cd674494e9451
Vlad <nightowlswing@gmail.com>
2020-01-22 11:42:06 +0200

    merging conflicts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Recorder from './components/Recorder/Recorder';
 import './App.css';
-import './styles/RecorderStyle/Record.css';
 import 'typeface-roboto';
 import Button from '@material-ui/core/Button';
 
@@ -13,13 +12,8 @@ class App extends Component {
         backgroundColor: 'linear-gradient(to top, rgba(51, 80, 102), rgba(21,32,41))'
       }}
       >
-<<<<<<< HEAD
         <div >
           <Recorder/>
-=======
-        <div className='Recorder'>
-          <Recorder></Recorder>
->>>>>>> 07a12ab06d1239c7935a8e46c9f4a7d9ab100b85
         </div>
       </div>
     );
