6215108777e4eed83ce73396ec64238a438b4ea2
Berk <alexeym00@gmail.com>
2020-01-23 22:34:51 +0200

    fix audd: test, lyrics

--- src/components/Recorder/Recorder.jsx
+++ src/components/Recorder/Recorder.jsx
@@ -6,11 +6,11 @@ import CreateRecord from './CreateRecord';
 import '../../styles/GamesStyles/Game.css';
 import auddIO from '../../requests/audd';
 
+
 const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
 class Recorder extends Component {
   audd = new auddIO();
-
   constructor(props) {
     super(props);
     this.state = {
@@ -22,7 +22,8 @@ class Recorder extends Component {
       isTextChosen: false,
       attemptsLeft: 5,
       hasWon: false,
-      hasLost: false
+      hasLost: false,
+      response: ''
     }
   }
 
@@ -38,60 +39,71 @@ class Recorder extends Component {
     }
   };
 
+  handleResponse = (response) => {
+    //const resault = props.resault,
+    const res = response;
+    this.setState({
+      response: res,
+    }, () => {
+      console.log(this.state.response)
+    });
+    ;
+  }
+
   stop = () => {
     Mp3Recorder
       .stop()
       .getMp3()
       .then(([buffer, blob]) => {
-        
+
         const blobURL = URL.createObjectURL(blob);
         this.setState({ blobURL, isRecording: false });
       })
-        .catch((e) => console.log(e));
-        this.setState({isRecorded: true});
+      .catch((e) => console.log(e));
+    this.setState({ isRecorded: true });
 
-  
-  // const file = new File(buffer, 'me-at-thevoice.mp3', {
-  //   type: blob.type,
-  //   lastModified: Date.now()
-  // });
-  // console.log(file);
-  // console.log(blob);
-  // console.log(buffer);
-  // const urlFile = URL.createObjectURL(file);
-  // this.setState({ urlFile, isRecording: false });
-  // console.log(urlFile);
-  // const player = new Audio(urlFile);
-  // player.play();
-  //     }).catch((e) => console.log(e));
-  //     this.setState({isRecorded: true})
-      };
 
-      rewrite = () => {
-        this.setState(
-          {
-            isRecording: false,
-            blobURL: '',
-            isBlocked: false,
-            isRecorded: false,
-            
-          }
-        );
+    // const file = new File(buffer, 'me-at-thevoice.mp3', {
+    //   type: blob.type,
+    //   lastModified: Date.now()
+    // });
+    // console.log(file);
+    // console.log(blob);
+    // console.log(buffer);
+    // const urlFile = URL.createObjectURL(file);
+    // this.setState({ urlFile, isRecording: false });
+    // console.log(urlFile);
+    // const player = new Audio(urlFile);
+    // player.play();
+    //     }).catch((e) => console.log(e));
+    //     this.setState({isRecorded: true})
+  };
+
+  rewrite = () => {
+    this.setState(
+      {
+        isRecording: false,
+        blobURL: '',
+        isBlocked: false,
+        isRecorded: false,
+
       }
-  
+    );
+  }
+
   handleSubmit = () => {
     const attempts = this.state.attemptsLeft;
     //Когда будет запрос на сайт, его нужно сюда писать и здесь же проводить анализ угадал сайт или не угадал. Если угадал, то делаем hasWon - тру
-    console.log(await this.audd.sendTest())
+    this.audd.sendTest(this.handleResponse)
     // console.log(this.state.blobURL);
 
-    if (!this.state.hasWon){
+    if (!this.state.hasWon) {
       this.setState(
-        {attemptsLeft: attempts - 1}
-        )
-      if(attempts - 1 === 0){
+        { attemptsLeft: attempts - 1 }
+      )
+      if (attempts - 1 === 0) {
         this.props.endGame(this.state.hasWon);
-      }else{
+      } else {
         this.setState({
           isRecording: false,
           blobURL: '',
@@ -99,50 +111,50 @@ class Recorder extends Component {
           isRecorded: false,
         })
       }
-    }else{
+    } else {
       this.props.endGame(this.state.hasWon);
     }
 
   }
 
-  componentDidMount(){  
+  componentDidMount() {
     navigator.getUserMedia = (
       navigator.getUserMedia ||
       navigator.webkitGetUserMedia ||
       navigator.mozGetUserMedia ||
       navigator.msGetUserMedia
-  );
-  
+    );
+
     if (typeof navigator.mediaDevices.getUserMedia === 'undefined') {
-        navigator.getUserMedia({
-            audio: true
-        },() => {
-          console.log('Permission Denied');
-          this.setState({ isBlocked: true })
-        });
+      navigator.getUserMedia({
+        audio: true
+      }, () => {
+        console.log('Permission Denied');
+        this.setState({ isBlocked: true })
+      });
 
     } else {
-        navigator.mediaDevices.getUserMedia({
-            audio: true
-        },() => {
-          console.log('Permission Denied');
-          this.setState({ isBlocked: true })
-        })  
+      navigator.mediaDevices.getUserMedia({
+        audio: true
+      }, () => {
+        console.log('Permission Denied');
+        this.setState({ isBlocked: true })
+      })
     }
   }
   render() {
     return (
-      <div className = 'Recorder'> 
-        <CreateRecord isRecorded={this.state.isRecorded} isRecording={this.state.isRecording} start={this.start} stop={this.stop} isRecording={this.state.isRecording}/>
-        <ListenRecord isRecorded={this.state.isRecorded} src={this.state.blobURL} rewrite = {this.rewrite} handleSubmit = {this.handleSubmit}/>
-        <AttemptsNumber attempts = {this.state.attemptsLeft}/>
+      <div className='Recorder'>
+        <CreateRecord isRecorded={this.state.isRecorded} isRecording={this.state.isRecording} start={this.start} stop={this.stop} isRecording={this.state.isRecording} />
+        <ListenRecord isRecorded={this.state.isRecorded} src={this.state.blobURL} rewrite={this.rewrite} handleSubmit={this.handleSubmit} />
+        <AttemptsNumber attempts={this.state.attemptsLeft} />
       </div>
     );
   }
 }
 
-const AttemptsNumber = props =>(
-  <div className = 'textAttempts'>
+const AttemptsNumber = props => (
+  <div className='textAttempts'>
     you have {`${props.attempts}`} attempts left
   </div>
 )
---
+++ src/requests/testResponse.jsx
@@ -0,0 +1,126 @@
+const testResponse = {
+    "status": "success",
+    "result": {
+        "artist": "Imagine Dragons",
+        "title": "Warriors",
+        "album": "Warriors",
+        "release_date": "2014-09-18",
+        "label": "Universal Music",
+        "timecode": "00:40",
+        "deezer": {
+            "id": 96398260,
+            "readable": true,
+            "title": "Warriors",
+            "title_short": "Warriors",
+            "title_version": "",
+            "link": "https://www.deezer.com/track/96398260",
+            "duration": 172,
+            "rank": 237633,
+            "explicit_lyrics": false,
+            "preview": "https://cdns-preview-4.dzcdn.net/stream/c-47629ba6ee1ffc1c60979df64e870041-1.mp3",
+            "artist": {
+                "id": 416239,
+                "name": "Imagine Dragons",
+                "link": "https://www.deezer.com/artist/416239",
+                "picture": "https://api.deezer.com/2.0/artist/416239/image",
+                "picture_small": "https://cdns-images.dzcdn.net/images/artist/a9087f289c3bd826eb8c8bd48cd11f1e/56x56-000000-80-0-0.jpg",
+                "picture_medium": "https://cdns-images.dzcdn.net/images/artist/a9087f289c3bd826eb8c8bd48cd11f1e/250x250-000000-80-0-0.jpg",
+                "picture_big": "https://cdns-images.dzcdn.net/images/artist/a9087f289c3bd826eb8c8bd48cd11f1e/500x500-000000-80-0-0.jpg",
+                "picture_xl": "https://cdns-images.dzcdn.net/images/artist/a9087f289c3bd826eb8c8bd48cd11f1e/1000x1000-000000-80-0-0.jpg",
+                "tracklist": "https://api.deezer.com/2.0/artist/416239/top?limit=50",
+                "type": "artist"
+            },
+            "album": {
+                "id": 9809740,
+                "title": "Insurgent (Original Motion Picture Soundtrack)",
+                "cover": "https://api.deezer.com/2.0/album/9809740/image",
+                "cover_small": "https://cdns-images.dzcdn.net/images/cover/b14d29ffcdf7ed9e8092d59de067bc6c/56x56-000000-80-0-0.jpg",
+                "cover_medium": "https://cdns-images.dzcdn.net/images/cover/b14d29ffcdf7ed9e8092d59de067bc6c/250x250-000000-80-0-0.jpg",
+                "cover_big": "https://cdns-images.dzcdn.net/images/cover/b14d29ffcdf7ed9e8092d59de067bc6c/500x500-000000-80-0-0.jpg",
+                "cover_xl": "https://cdns-images.dzcdn.net/images/cover/b14d29ffcdf7ed9e8092d59de067bc6c/1000x1000-000000-80-0-0.jpg",
+                "tracklist": "https://api.deezer.com/2.0/album/9809740/tracks",
+                "type": "album"
+            },
+            "type": "track"
+        },
+        "spotify": {
+            "album": {
+                "album_type": "album",
+                "artists": [
+                    {
+                        "external_urls": {
+                            "spotify": "https://open.spotify.com/artist/53XhwfbYqKCa1cC15pYq2q"
+                        },
+                        "href": "https://api.spotify.com/v1/artists/53XhwfbYqKCa1cC15pYq2q",
+                        "id": "53XhwfbYqKCa1cC15pYq2q",
+                        "name": "Imagine Dragons",
+                        "type": "artist",
+                        "uri": "spotify:artist:53XhwfbYqKCa1cC15pYq2q"
+                    }
+                ],
+                "available_markets": null,
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/album/6ecx4OFG0nlUMqAi9OXQER"
+                },
+                "href": "https://api.spotify.com/v1/albums/6ecx4OFG0nlUMqAi9OXQER",
+                "id": "6ecx4OFG0nlUMqAi9OXQER",
+                "images": [
+                    {
+                        "height": 640,
+                        "url": "https://i.scdn.co/image/c6f5292d63c9de52f99dd5cef273a7840c3bb40f",
+                        "width": 640
+                    },
+                    {
+                        "height": 300,
+                        "url": "https://i.scdn.co/image/bb562520f8ebef91dda98e74f997f5b7d29a05e4",
+                        "width": 300
+                    },
+                    {
+                        "height": 64,
+                        "url": "https://i.scdn.co/image/3032a960eda137e082f807434cf5fa85dee0f136",
+                        "width": 64
+                    }
+                ],
+                "name": "Smoke + Mirrors (Deluxe)",
+                "release_date": "2015-10-30",
+                "release_date_precision": "day",
+                "total_tracks": 21,
+                "type": "album",
+                "uri": "spotify:album:6ecx4OFG0nlUMqAi9OXQER"
+            },
+            "artists": [
+                {
+                    "external_urls": {
+                        "spotify": "https://open.spotify.com/artist/53XhwfbYqKCa1cC15pYq2q"
+                    },
+                    "href": "https://api.spotify.com/v1/artists/53XhwfbYqKCa1cC15pYq2q",
+                    "id": "53XhwfbYqKCa1cC15pYq2q",
+                    "name": "Imagine Dragons",
+                    "type": "artist",
+                    "uri": "spotify:artist:53XhwfbYqKCa1cC15pYq2q"
+                }
+            ],
+            "available_markets": null,
+            "disc_number": 1,
+            "duration_ms": 170066,
+            "explicit": false,
+            "external_ids": {
+                "isrc": "USUM71414163"
+            },
+            "external_urls": {
+                "spotify": "https://open.spotify.com/track/1lgN0A2Vki2FTON5PYq42m"
+            },
+            "href": "https://api.spotify.com/v1/tracks/1lgN0A2Vki2FTON5PYq42m",
+            "id": "1lgN0A2Vki2FTON5PYq42m",
+            "is_local": false,
+            "name": "Warriors",
+            "popularity": 68,
+            "preview_url": "",
+            "track_number": 18,
+            "type": "track",
+            "uri": "spotify:track:1lgN0A2Vki2FTON5PYq42m"
+        }
+    }
+};
+
+export default testResponse;
